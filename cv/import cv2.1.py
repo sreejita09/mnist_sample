d629f677f7eb2557ea60f0f8a054696fccd704a1
sreejita09 <sreejitabhardwaj2005@gmail.com>
2025-09-02 18:32:01 +0530

    Remove cv folder from repository but keep locally

--- cv/import cv2.1.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-# Read the image
-img = cv2.imread("C:\Users\DELL\OneDrive\Desktop\SELF STUDY\main quest\samples in cv\photos and other materials\food photo.jpg")  # Replace with your image path
-
-# Display the image
-cv2.imshow('Image', img)
-cv2.waitKey(0)  # Wait for a key press to close the window
-cv2.destroyAllWindows()
